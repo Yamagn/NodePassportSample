421d25697227cddf6cb2c6c17ec1bd173b86f8a0
f_takahashi <takahashi@trifort.jp>
2015-06-08 18:26:21 +0900

    commit

--- app.js
+++ app.js
@@ -6,13 +6,79 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
+
+//---------ここからpassport-----------
+
+//エラー用フラッシュメッセージのモジュールを設定
+var flash = require('connect-flash');
+app.use(flash());
+
+//passportの初期化
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy;
+app.use(require('express-session')({
+    secret: 'fjaiofjfiafkldsfkadjkafk',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//認証ロジック
+passport.use(
+	new LocalStrategy(
+		{
+			//Viewのアカウント入力フォームのname属性を指定する
+			usernameField: 'user_name', 
+			passwordField: 'user_password'
+		},
+		function(username, password, done) {
+			//routerのpassport.authenticate()が呼ばれたらここの処理が走る。
+
+			if(username == 'myname' && password == 'mypassword'){
+				//ログイン成功
+				//今回はここの判定式をハードコーディングにする
+        		return done(null, username);
+			}
+
+			//ログイン失敗
+			//messageはログイン失敗時のフラッシュメッセージ。
+			//各routerの req.flash() で取得できる。
+       		return done(null, false, {message:'ID or Passwordが間違っています。'});
+		}
+	)
+);
+
+
+//認証した際のオブジェクトをシリアライズしてセッションに保存する。
+passport.serializeUser(function(username, done) {
+	console.log('serializeUser');
+	done(null, username);
+});
+
+
+//認証時にシリアライズしてセッションに保存したオブジェクトをデシリアライズする。
+//デシリアライズしたオブジェクトは各routerの req.user で参照できる。
+passport.deserializeUser(function(username, done) {
+	console.log('deserializeUser');
+	done(null, {name:username, msg:'my message'});
+});
+
+//---------ここまでpassport-----------
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+////app.set('view engine', 'jade');
+
+var ECT = require('ect');
+var ectRenderer = ECT({ watch: true, root: __dirname + '/views', ext : '.ect' });
+
+app.set('view engine', 'ect');
+app.engine('ect', ectRenderer.render);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -23,7 +89,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,69 @@
 var express = require('express');
 var router = express.Router();
 
+var passport = require('passport');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+	//ログイン時のエラーメッセージ表示
+	var errorMsg = '';
+	var error = req.flash().error;
+	if(error){
+		var errorMsg = error[0];
+	}
+
+	res.render('index', { title: 'Express', msg: errorMsg });
+});
+
+
+//ログイン処理
+router.post(
+	'/login', 
+	passport.authenticate( 'local', { successRedirect: '/mypage', failureRedirect: '/', failureFlash: true }), 
+	function(req, res, next) {
+		//認証成功した場合はマイページにリダイレクト
+		res.redirect('/mypage');
+	}
+);
+
+
+//マイページ（ログイン後のページで認証済みじゃないと見れない）
+router.get('/mypage', function(req, res, next) {
+
+	//log
+	console.log('mypage');
+	console.log(req.user);
+
+	if(req.user){
+		//userが存在する場合は認証済み
+		res.render('mypage', { title: 'Mypage' });
+	}else{
+		res.redirect('/');
+	}
+});
+
+
+//設定画面（認証済みじゃないと見れない）
+router.get('/setting', function(req, res, next) {
+
+	//log
+	console.log('mypage');
+	console.log(req.user);
+
+	if(req.user){
+		//userが存在する場合は認証済み
+		res.render('setting', { title: 'Setting' });
+	}else{
+		res.redirect('/');
+	}
+});
+
+
+//ログアウト
+router.get('/logout', function(req, res, next) {
+	req.logout();
+	res.redirect('/');
 });
 
 module.exports = router;
